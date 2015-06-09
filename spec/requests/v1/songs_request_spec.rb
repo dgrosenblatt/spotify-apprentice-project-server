d104e0d9adc544782a885d3820fce5e26c49415f
Danny Rosenblatt <dgrosenblatt@gmail.com>
2015-06-09 15:13:23 -0400

    Add GET /songs request spec

---
+++ spec/requests/v1/songs_request_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+describe 'songs endpoints' do
+  describe 'GET /index' do
+    it 'returns JSON for all songs' do
+      songs = create_list(:songs, 10)
+      get(songs_url, {}, accept_headers)
+      expect(response).to have_http_status :ok
+      expect(response).to match_response_schema :songs
+    end
+  end
+end
