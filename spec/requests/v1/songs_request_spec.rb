1b63d36d191ad6182ebc9844cf18cf164fb55497
Danny Rosenblatt <dgrosenblatt@gmail.com>
2015-06-10 10:42:07 -0400

    Reorganize GET /index tests in songs request spec into separate context blocks

--- spec/requests/v1/songs_request_spec.rb
+++ spec/requests/v1/songs_request_spec.rb
@@ -2,17 +2,23 @@ require 'rails_helper'
 
 describe 'songs endpoints' do
   describe 'GET /index' do
-    it 'returns JSON for all songs' do
-      songs = create_list(:song, 10)
-      get(songs_url, {}, accept_headers)
-      expect(response).to have_http_status :ok
-      expect(response).to match_response_schema :songs
+    context 'when there are many songs' do
+      it 'returns JSON for all songs' do
+        songs = create_list(:song, 10)
+        get(songs_url, {}, accept_headers)
+
+        expect(response).to have_http_status :ok
+        expect(response).to match_response_schema :songs
+      end
     end
 
-    it 'returns an empty list of songs' do
-      get(songs_url, {}, accept_headers)
-      expect(response).to have_http_status :ok
-      expect(response).to match_response_schema :songs
+    context 'when there are no songs' do
+      it 'returns an empty list of songs' do
+        get(songs_url, {}, accept_headers)
+
+        expect(response).to have_http_status :ok
+        expect(response).to match_response_schema :songs
+      end
     end
   end
 end
