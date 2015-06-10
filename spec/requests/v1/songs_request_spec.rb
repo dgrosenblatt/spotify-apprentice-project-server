0cf49d37ab0160b8aed8c5c294cec10c65a64150
Danny Rosenblatt <dgrosenblatt@gmail.com>
2015-06-10 13:14:11 -0400

    Add GET /songs/:id request spec

--- spec/requests/v1/songs_request_spec.rb
+++ spec/requests/v1/songs_request_spec.rb
@@ -1,10 +1,11 @@
 require 'rails_helper'
 
 describe 'songs endpoints' do
-  describe 'GET /index' do
+  describe 'GET /songs' do
     context 'when there are many songs' do
       it 'returns JSON for all songs' do
-        songs = create_list(:song, 10)
+        create_list(:song, 10)
+
         get(songs_url, {}, accept_headers)
 
         expect(response).to have_http_status :ok
@@ -19,6 +20,17 @@ describe 'songs endpoints' do
         expect(response).to have_http_status :ok
         expect(response).to match_response_schema :songs
       end
+    end
+  end
+
+  describe 'GET /songs/:id' do
+    it 'returns JSON for one song' do
+      song = create(:song)
+
+      get(song_url(song), {}, accept_headers)
+
+      expect(response).to have_http_status :ok
+      expect(response).to match_response_schema :song
     end
   end
 end
