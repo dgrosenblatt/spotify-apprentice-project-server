883efec6f7280e1d522bba294c73fc5967b711ba
Danny Rosenblatt <dgrosenblatt@gmail.com>
2015-06-11 15:12:13 -0400

    Change duration in song serializer to use duration in minutes and seconds

--- app/controllers/v1/songs_controller.rb
+++ app/controllers/v1/songs_controller.rb
@@ -1,11 +1,11 @@
 class V1::SongsController < ApplicationController
   def index
-    songs = Song.includes(:artist)
+    songs = Song.includes(:artist).decorate
     render json: songs, include: [:artist]
   end
 
   def show
-    song = Song.find(params[:id])
+    song = Song.find(params[:id]).decorate
     render json: song, include: [:artist]
   end
 end
--- app/serializers/song_serializer.rb
+++ app/serializers/song_serializer.rb
@@ -1,5 +1,11 @@
 class SongSerializer < ActiveModel::Serializer
   attributes :id, :title, :album, :duration
+
   embed :ids
+
   has_one :artist
+
+  def duration
+    object.duration_in_minutes_and_seconds
+  end
 end
