34f9630bf054ece9c490bab68d07143ae793d949
Danny Rosenblatt <dgrosenblatt@gmail.com>
2015-06-11 14:20:05 -0400

    Add destroy dependency for songs to artist model

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,5 +1,5 @@
 class Artist < ActiveRecord::Base
-  has_many :songs
+  has_many :songs, dependent: :destroy
 
   validates :name, presence: true
   validates :spotify_uri, presence: true, uniqueness: true
