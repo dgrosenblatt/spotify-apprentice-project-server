096c8ee9f32b0b354c644908d67022706a046716
Danny Rosenblatt <dgrosenblatt@gmail.com>
2015-06-11 14:28:05 -0400

    Reorganize song, artist, and model specs according to Intrepid style guide

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,9 +1,9 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
-
-  validates :title, presence: true, uniqueness: { scope: :album }
-  validates :artist, presence: true
+  
   validates :album, presence: true
+  validates :artist, presence: true
   validates :duration, presence: true, numericality: { greater_than: 0 }
   validates :spotify_uri, presence: true, uniqueness: true
+  validates :title, presence: true, uniqueness: { scope: :album }
 end
--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
 describe Artist do
+  describe 'associations' do
+    it { should have_many :songs }
+  end
+  
   describe 'validations' do
     it { should validate_presence_of :name }
     it { should validate_presence_of :spotify_uri }
     it { should validate_uniqueness_of :spotify_uri }
-  end
-
-  describe 'associations' do
-    it { should have_many :songs }
   end
 end
