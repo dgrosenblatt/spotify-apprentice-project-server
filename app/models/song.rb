243f54d6fca87602ef62af8bf2e01fe2a5497819
Danny Rosenblatt <dgrosenblatt@gmail.com>
2015-06-11 08:29:23 -0400

    Revise song validation to use artist object instead of id

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,7 +2,7 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   validates :title, presence: true, uniqueness: { scope: :album }
-  validates :artist_id, presence: true
+  validates :artist, presence: true
   validates :album, presence: true
   validates :duration, presence: true, numericality: { greater_than: 0 }
   validates :spotify_uri, presence: true, uniqueness: true
--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -4,7 +4,7 @@ describe Song do
   describe 'validations' do
     it { should validate_presence_of :title }
     it { should validate_uniqueness_of(:title).scoped_to(:album) }
-    it { should validate_presence_of :artist_id }
+    it { should validate_presence_of :artist }
     it { should validate_presence_of :album }
     it { should validate_presence_of :duration }
     it { should validate_numericality_of(:duration).is_greater_than(0) }
