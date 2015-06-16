1dfb92baf39784c8862d4d727bee2758552237e2
Danny Rosenblatt <dgrosenblatt@gmail.com>
2015-06-16 13:23:30 -0400

    Sideload genres and artists

--- app/controllers/v1/mixtapes_controller.rb
+++ app/controllers/v1/mixtapes_controller.rb
@@ -2,6 +2,6 @@ class V1::MixtapesController < ApplicationController
   def show
     mixtape = MixtapeGenerator.perform(genre_name: params[:genre])
 
-    render json: mixtape
+    render json: mixtape, include: [:artist, :genre]
   end
 end
