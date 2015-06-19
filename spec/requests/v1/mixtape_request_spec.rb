8f1e59ccf7e196a0ce67c56f72e21df3d1d120d0
Danny Rosenblatt <dgrosenblatt@gmail.com>
2015-06-19 15:17:10 -0400

    Clean up mixtape request spec and mixtape json schema

--- spec/requests/v1/mixtape_request_spec.rb
+++ spec/requests/v1/mixtape_request_spec.rb
@@ -2,47 +2,71 @@ require 'rails_helper'
 
 describe 'mixtape endpoints' do
   describe 'GET /mixtape' do
-    before(:each) do
-      allow(MixtapeGenerator).to receive(:perform)
+    before do
+      genre = create(:genre)
+      create_list(:song, 10, genre: genre)
+      @genre_name = genre.name
     end
-    context 'with one genre specified' do
+
+    context 'when a genre and time are specified' do
       it 'responds with JSON for a mixtape' do
-        get(mixtape_url, { genre: 'blues' }, accept_headers)
+        get(mixtape_url, { genre: @genre_name, time: 2000 }, accept_headers)
 
         expect(response).to have_http_status :ok
         expect(response).to match_response_schema :mixtape
       end
       it 'creates a mixtape' do
-        get(mixtape_url, { genre: 'blues' }, accept_headers)
+        allow(MixtapeGenerator).to receive(:perform)
+
+        get(mixtape_url, { genre: @genre_name, time: 2000 }, accept_headers)
 
         expect(MixtapeGenerator).to have_received(:perform)
       end
     end
 
-    context 'with a genre and time in seconds specified' do
+    context 'when only a genre is specified' do
       it 'responds with JSON for a mixtape' do
-        get(mixtape_url, { genre: 'blues', time: 3600 }, accept_headers)
+        get(mixtape_url, { genre: @genre_name }, accept_headers)
 
         expect(response).to have_http_status :ok
         expect(response).to match_response_schema :mixtape
       end
       it 'creates a mixtape' do
-        get(mixtape_url, { genre: 'blues' }, accept_headers)
+        allow(MixtapeGenerator).to receive(:perform)
+
+        get(mixtape_url, { genre: @genre_name }, accept_headers)
 
         expect(MixtapeGenerator).to have_received(:perform)
       end
     end
 
-    context 'without genre or time specified' do
+    context 'when only a time is specified' do
       it 'responds with a mixtape for a randomly selected genre' do
-        create_list(:song, 10)
+        get(mixtape_url, { time: 2000 }, accept_headers)
+
+        expect(response).to have_http_status :ok
+        expect(response).to match_response_schema :mixtape
+      end
+      it 'creates a mixtape' do
+        allow(MixtapeGenerator).to receive(:perform)
+
+        get(mixtape_url, { time: 2000 }, accept_headers)
+
+        expect(MixtapeGenerator).to have_received(:perform)
+      end
+    end
+
+    context 'when neither a genre nor time specified' do
+      it 'responds with a mixtape for a randomly selected genre' do
         get(mixtape_url, { }, accept_headers)
 
         expect(response).to have_http_status :ok
         expect(response).to match_response_schema :mixtape
       end
       it 'creates a mixtape' do
-        get(mixtape_url, { genre: 'blues' }, accept_headers)
+        allow(MixtapeGenerator).to receive(:perform)
+
+        get(mixtape_url, { }, accept_headers)
 
         expect(MixtapeGenerator).to have_received(:perform)
       end
